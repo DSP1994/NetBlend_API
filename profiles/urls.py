c0659d6871d8999d255963dc3636386868d0f86a
David <parrydavid@live.co.uk>
2023-03-10 22:33:39 +0000

    updated urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from profiles import views
 
 # url links for profiles
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view())
+    path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view())
 ]

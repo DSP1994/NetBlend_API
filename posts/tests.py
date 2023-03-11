8e1a0e9a7cec6870f44c368c394dcde9f4d1ad85
David <parrydavid@live.co.uk>
2023-03-11 01:11:41 +0000

    created code that will fail

--- posts/tests.py
+++ posts/tests.py
@@ -27,3 +27,26 @@ class PostListViewTest(APITestCase):
     def test_logged_out_user_cannot_create_post(self):
         response = self.client.post('/posts/', {'title': 'test title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='david', password='password2')
+        User.objects.create_user(username='brain', password='password2')
+        Post.objects.create(
+            owner=david, title='test title', content='test content'
+        )
+        Post.objects.create(
+            owner=brian, title='test title 2', content='test content 2'
+        )
+
+    def user_can_retrieve_post_with_valid_id(self):
+        response = self.client.get('/posts/1')
+        self.assertEqual(response.data['title'], 'test title')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # def user_cannot_retrieve_post_with_an_invalid_id
+
+    # def user_can_update_any_posts_they_own
+
+    # def user_cannot_anyone_elses_posts

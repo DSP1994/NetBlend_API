62651b26047eb3be069c464e905b67491e4a622c
David <parrydavid@live.co.uk>
2023-03-11 15:46:46 +0000

    created serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,3 +2,8 @@ from rest_framework import serializers
 from .models import Comment
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image')

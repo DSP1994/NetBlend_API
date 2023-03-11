68adea280f56f32658e7c32ef6b074f95e1bc020
David <parrydavid@live.co.uk>
2023-03-11 17:42:16 +0000

    added create function to like serializer to handle uniqueness of owner and post

--- likes/serializer.py
+++ likes/serializer.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -13,3 +14,11 @@ class LikeSerializer(serializers.ModelSerializer):
             'owner',
             'post',
         ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'This is a possible duplicate'
+            })

d707d0e827b140fd96b79d47de2f7edcb5cedb60
David <parrydavid@live.co.uk>
2023-03-11 15:56:03 +0000

    uncommented models.py

--- comments/models.py
+++ comments/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 from posts.models import Post
 
 
-# # Create your models here.
-# class Comment(models.Model):
-#     # This model will be related to User and Post (hence the imports)
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#     post = models.ForeignKey(User, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     content = models.TextField()
+# Create your models here.
+class Comment(models.Model):
+# This model will be related to User and Post (hence the imports)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
-#     class Meta:
-#         ordering = ['-created_at']
+    class Meta:
+        ordering = ['-created_at']
 
-#     def __str__(self):
-#         return self.content
+    def __str__(self):
+        return self.content

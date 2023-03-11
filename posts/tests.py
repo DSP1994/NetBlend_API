96d9126dfe5b80bddab6eed8a8ca90236a5bcbd6
David <parrydavid@live.co.uk>
2023-03-11 00:55:32 +0000

    created a test in posts tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+class PostListViewTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='david', passwords='pass')
+
+    def test_can_list_posts(self):
+        david = User.objects.get(username='david')
+        Post.objects.create(owner=david, title='test title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)

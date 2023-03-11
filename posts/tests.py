4bcb50181b34f30533d42118766ba7deb8d4ad52
David <parrydavid@live.co.uk>
2023-03-11 00:57:46 +0000

    created a successful test

--- posts/tests.py
+++ posts/tests.py
@@ -7,10 +7,12 @@ from rest_framework.test import APITestCase
 # Create your tests here.
 class PostListViewTest(APITestCase):
     def setUp(self):
-        User.objects.create_user(username='david', passwords='pass')
+        User.objects.create_user(username='david', password='password')
 
     def test_can_list_posts(self):
         david = User.objects.get(username='david')
         Post.objects.create(owner=david, title='test title')
         response = self.client.get('/posts/')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))

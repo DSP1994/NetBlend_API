a665c2f2b63482cc30e1b32f1dc3d48540cedacf
David <parrydavid@live.co.uk>
2023-03-12 10:53:12 +0000

    imported and used count/filters to profiles views to define specific fields

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from api_nb.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner_following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 

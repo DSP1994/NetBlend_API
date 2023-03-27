cdd8acd37268ea061e03aa232223e8d90c7e1495
David <parrydavid@live.co.uk>
2023-03-27 13:52:05 +0000

    added trailing slash to posts url in hopes to fix error

--- nb-apps/posts/urls.py
+++ nb-apps/posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 # url links for posts
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>', views.PostDetail.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]

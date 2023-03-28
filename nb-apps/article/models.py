1ba09dffffce0122e57e8a7ff830c9bd5e022893
David <parrydavid@live.co.uk>
2023-03-28 18:31:46 +0000

    created article class in models.py

--- nb-apps/article/models.py
+++ nb-apps/article/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    content = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.title}'

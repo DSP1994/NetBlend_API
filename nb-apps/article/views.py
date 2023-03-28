bc7f1ee85f280466b7ccf19786508970f19dae0a
David <parrydavid@live.co.uk>
2023-03-28 18:34:54 +0000

    created views.py article list

--- nb-apps/article/views.py
+++ nb-apps/article/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from api_nb.permissions import IsOwnerOrReadOnly
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleList(generics.ListCreateAPIView):
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Article.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Article.objects.all()

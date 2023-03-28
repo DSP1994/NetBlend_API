4d49cbfdb6c25758817c839eebd5d36c9cdb4080
David <parrydavid@live.co.uk>
2023-03-28 18:17:07 +0000

    created views.py with for the event list

--- nb-apps/events/views.py
+++ nb-apps/events/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from api_nb.permissions import IsOwnerOrReadOnly
+from .models import Event
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
+
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+        'city',
+        'country',
+    ]
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Update & Destroy events.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()

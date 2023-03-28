56dea3ada9bdeebc7da7e70b9bd842fda4336ab8
David <parrydavid@live.co.uk>
2023-03-28 18:39:18 +0000

    created profile image in serializers.py for articles

--- nb-apps/article/serializers.py
+++ nb-apps/article/serializers.py
@@ -6,6 +6,23 @@ class ArticleSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image must be smaller than 2MB.')
+
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Height must be smaller than 4096px.')
+
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Width must be smaller than 4096px.')
+
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
